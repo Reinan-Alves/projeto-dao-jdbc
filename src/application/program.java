48a4edc2ebb178ea0029af3e60b1944bd64ba8ca
unknown <reinanalves1988@gmail.com>
2022-09-14 11:54:46 -0300

    Reusing instatiation

--- src/application/program.java
+++ src/application/program.java
@@ -2,16 +2,15 @@ package application;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class program {
 
 	public static void main(String[] args) {
-		Department obj = new Department(1, "Books");
-
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		System.out.println("=== TEST 1: seller findById ====");
 		
 		Seller seller = sellerDao.findById(3);
 		
